d403a90bd2cadd875a6c9b3995f4860441f1dc48
Arjunren Von <arjunrenvon@gmail.com>
2026-04-12 19:51:41 +0800

    Update

--- data.js
+++ data.js
@@ -3,31 +3,31 @@ const flowers = [
         id: 1,
         title: "Azure Dawn Bouquet",
         picture: "./product_one.jpg",
-        price: "₱299.00"
+        price: "₱280.00"
     },
     {
         id: 2,
-        title: "Sunset Mirage Bouquet",
+        title: "Sundrop Bloom",
         picture: "./product_two.jpg",
-        price: "₱529.00"
+        price: "₱499.00"
     },
     {
         id: 3,
-        title: "Amethyst Dreamscape",
+        title: "Amethyst Bloom",
         picture: "./product_three.jpg",
-        price: "₱529.00"
+        price: "₱499.00"
     },
     {
         id: 4,
         title: "Sapphire Spring Tulips",
         picture: "./product_four.jpg",
-        price: "₱549.00"
+        price: "₱499.00"
     },
     {
         id: 5,
-        title: " Oceanic Elegance Lily",
+        title: "Moonlit Bloom",
         picture: "./product_five.jpg",
-        price: "₱529.00"
+        price: "₱499.00"
     },
     {
         id: 6,
@@ -40,5 +40,71 @@ const flowers = [
         title: "The Velvet Daisy Duo",
         picture: "./product_seven.jpg",
         price: "₱99.00"
-    }
+    },
+    {
+        id: 8,
+        title: "Enchanted Verdantia ",
+        picture: "./product_eight.jpg",
+        price: "₱499.00"
+    },
+    {
+        id: 9,
+        title: "Fleur",
+        picture: "./product_nine.jpg",
+        price: "₱449.00"
+    },
+    {
+        id: 10,
+        title: "Verdant Bliss",
+        picture: "./product_ten.jpg",
+        price: "₱420.00"
+    },
+    {
+        id: 11,
+        title: "Eterna Rosa",
+        picture: "./product_eleven.jpg",
+        price: "₱429.00"
+    },
+    {
+        id: 12,
+        title: "Dainty Flor",
+        picture: "./product_twelve.jpg",
+        price: "₱99.00"
+    },
+    {
+        id: 13,
+        title: "Azie",
+        picture: "./product_thirteen.jpg",
+        price: "₱449.00"
+    },
+    {
+        id: 14,
+        title: "Flor de Luxe",
+        picture: "./product_fourteen.jpg",
+        price: "₱499.00"
+    },
+    {
+        id: 15,
+        title: "Sol Amor",
+        picture: "./product_fifteen.jpg",
+        price: "₱350.00"
+    },
+    {
+        id: 16,
+        title: "Pink Reverie",
+        picture: "./product_sixteen.jpg",
+        price: "₱280.00"
+    },
+    {
+        id: 17,
+        title: "Lil' Amor",
+        picture: "./product_seventeen.jpg",
+        price: "₱120.00"
+    },
+    {
+        id: 18,
+        title: "Pink Opaline",
+        picture: "./product_eighteen.jpg",
+        price: "₱399.00"
+    },
 ];
